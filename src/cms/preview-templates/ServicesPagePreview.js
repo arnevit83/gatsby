144f99a263e6fdaf0db3afbcd308e0385809c0a7
Paul Sparkes <paul.sparkes@live.com>
2021-01-15 22:51:23 +0000

    adding in drop down selection to the services page

---
+++ src/cms/preview-templates/ServicesPagePreview.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ServicesPageTemplate } from '../../templates/services-page'
+
+const ServicesPagePreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <ServicesPageTemplate
+        title={data.title}
+        description={data.description}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
+
+ServicesPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default ServicesPagePreview
--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -4,10 +4,9 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const ServicesPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
-  return (
+export const ServicesPageTemplate = ({
+  title,
+  description }) => (
     <section className="section section--gradient">
       <div className="container">
         <div className="columns">
@@ -16,47 +15,51 @@ export const ServicesPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
+              
+              <p>{description}</p>
             </div>
           </div>
         </div>
       </div>
     </section>
   )
-}
 
 ServicesPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
+  description: PropTypes.string,
 }
 
 const ServicesPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <ServicesPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
+        title={frontmatter.title}
+        description={frontmatter.description}
       />
     </Layout>
   )
 }
 
 ServicesPage.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
 }
 
 export default ServicesPage
 
-export const ServicesPageQuery = graphql`
-  query ServicesPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+export const pageQuery = graphql`
+  query ServicesPageQuery {
+    markdownRemark(frontmatter: { templateKey: { eq: "services-page" } }) {
       html
       frontmatter {
         title
+        description
       }
     }
   }
