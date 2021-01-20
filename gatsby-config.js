a63bc52617459c90b9c3442958f883e0198a8e06
arnevit83 <33792908+arnevit83@users.noreply.github.com>
2021-01-20 10:12:29 +0000

    Revert "Argon theme installed - with test button"
    This reverts commit b7c4ff0fd6ebc26ea7560534b07a44a2d090309b.

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,13 +42,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/assets`,
-        name: 'assets',
-      },
-    },
-    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [{
