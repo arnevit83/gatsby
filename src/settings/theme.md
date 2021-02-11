6fa06a9b7e2b2774864fc7f927a550db01d2d119
arnevit83 <33792908+arnevit83@users.noreply.github.com>
2021-02-11 14:21:38 +0000

    Admin Colours

--- src/settings/theme.md
+++ src/settings/theme.md
@@ -1,6 +1,7 @@
 ---
+templateKey: themesettings
 font: https://fonts.googleapis.com/css2?family=Roboto
-color-default: "#172b4d"
-color-primary: "#5e72e4"
-color-secondary: "#f4f5f7"
+colordefault: "#172b4d"
+colorprimary: "#5e72e4"
+colorsecondary: "#f4f5f7"
 ---
---
+++ src/templates/themesettings.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+export const ThemeSettingsTemplate = () => {
+  return (
+    <div></div>
+  )
+}
+const ThemeSettings = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ThemeSettings
+
+export const ThemeSettingsQuery = graphql`
+  query ThemeSettingsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        font
+        colordefault
+        colorprimary
+        colorsecondary
+      }
+    }
+  }
+`
