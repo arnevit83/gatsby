2b7f88826f144f246960aeeb4e6e27be3332435e
Paul Sparkes <paul.sparkes@live.com>
2021-02-16 22:28:30 +0000

    services page update

--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -2,99 +2,105 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
-import { Badge, Container, Row, Col } from "reactstrap";
-import LeftAlign from '../components/custom-widgets/LeftAlign'
+import { Container, Row, Col } from "reactstrap";
 import CustomWidget from '../components/custom-widgets/CustomWidget'
-
 import {
-
-
   Card,
   CardBody
 } from "reactstrap";
 
-export const ServicesPageTemplate = ({
-  title,
-  description,
-  blurbs }) => (
+export class ServicesPageTemplate extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <>
-         <section className="upper">
-          <Container>
-            <Row>
-              <Col className="mx-auto" md="10">
-                <Card className="card-invoice">
-                  <CardBody>
-                    <Row>
-                      <Col xs="12"> 
-                  
+    console.log(props);
+  }
 
-                                                          <section className="section section--gradient">
-                                                            <div className="container">
-                                                              <div className="columns">
-                                                                <div className="column is-12">
-                                                                  <div className="section">
-                                                                    <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                                                                      {title}
-                                                                    </h2>
-                                                                    
-                                                                    <p>{description}</p>
+  render() {
+    return (
+    <section className="upper">
+    <Container>
+      <Row>
+        <Col className="mx-auto" md="10">
+          <Card className="card-invoice">
+            <CardBody>
+              <Row>
+                <Col xs="12">
 
-                                                                    <div className="section features-1">
+                <section className="section section--gradient">
+                  <div className="container">
+                    <div className="columns">
+                      <div className="column is-12">
+                        <div className="section">
+                          <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                            {this.props.title}
+                          </h2>
+                          
+                          <p>{this.props.description}</p>
 
-                                                                    {blurbs.map((blurb) => (
-                                                                      <CustomWidget key={blurb.text} {...blurb}></CustomWidget>
-                                                                    ))}
-                                                                    </div>
-                                                                  </div>
-                                                                </div>
-                                                              </div>
-                                                            </div>
-                                                          </section>
+                          <div className="section features-1">
+                          {this.props.sections.map((section, index) => (
+                            <CustomWidget key={index} {...section}></CustomWidget>
+                          ))}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
 
 
-      
-              </Col>
-                    </Row>
-                  </CardBody>
-                 </Card>
-              </Col>
-          
-            </Row>
-          </Container>
-        </section>
-       
 
-    </>
-
-  )
+        </Col>
+              </Row>
+            </CardBody>
+           </Card>
+        </Col>
+    
+      </Row>
+    </Container>
+  </section>
+    )
+  }
+}
 
 ServicesPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   description: PropTypes.string,
-  blurbs: PropTypes.arrayOf(
+  sections: PropTypes.arrayOf(
     PropTypes.shape({
       text: PropTypes.string,
-      align: PropTypes.string,
+      type: PropTypes.string,
       image: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
     })
   )
 }
 
-const ServicesPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+// const ServicesPage = ({ data }) => {
+//   const { frontmatter } = data.markdownRemark;
 
-  return (
-    <Layout>
-      <ServicesPageTemplate
-        title={frontmatter.title}
-        description={frontmatter.description}
-        blurbs={frontmatter.blurbs}
-      />
-    </Layout>
-  )
+// }
+
+
+export default class ServicesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    debugger;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <ServicesPageTemplate
+          title={this.props.data.markdownRemark.frontmatter.title}
+          description={this.props.data.markdownRemark.frontmatter.description}
+          sections={this.props.data.markdownRemark.frontmatter.sections}
+        />
+      </Layout>
+    )
+  }
 }
 
 ServicesPage.propTypes = {
@@ -105,7 +111,7 @@ ServicesPage.propTypes = {
   }),
 }
 
-export default ServicesPage
+// export default ServicesPage
 
 export const pageQuery = graphql`
   query ServicesPageQuery {
@@ -114,8 +120,8 @@ export const pageQuery = graphql`
       frontmatter {
         title
         description
-        blurbs {
-          align
+        sections {
+          type
           text
           image {
             childImageSharp {
