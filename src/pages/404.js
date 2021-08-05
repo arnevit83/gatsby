d7bc5c28ae6f95e09a01dfc4a10b74595e338e3e
arnevit83 <33792908+arnevit83@users.noreply.github.com>
2021-08-05 20:04:18 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import Layout from '../components/Layout'
-
+import React from "react";
+import { Link } from "gatsby";
+import { Container, Row, Col, Card, CardBody } from "reactstrap";
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+	<>
+		<section className="upper">
+			<Container fluid={true}>
+				<Row>
+					<Col className="mx-auto" md="10">
+						<Card className="card-invoice">
+							<CardBody>
+								<Container>
+									<div className="text-center">
+										<br></br>
+										<h4>404 Page not found</h4>
+										<br />
+										<Link to="/">
+											&#11164;&#11164;&#11164; Back to Homepage
+											&#11164;&#11164;&#11164;
+										</Link>
+										<br></br> <br></br>
+									</div>
+									<iframe
+										width="100%"
+										height="600px"
+										src="https://www.youtube.com/embed/7wfYIMyS_dI?autoplay=1&start=6"
+										title="YouTube video player"
+										frameborder="0"
+										allow="accelerometer; autoplay=true; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+										allowfullscreen
+									></iframe>
+								</Container>
+							</CardBody>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	</>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
